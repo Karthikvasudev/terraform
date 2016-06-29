2592ce9c6338908ba0995ada76d90aa6a983f883
Raghavan <rakesh.raghavan@abbott.com>
2016-06-29 14:30:57 -0700

    Added sec group rule to allow yum install in userdata script, also added config files

--- aip-stack/providers/aws/admin/admin.tf
+++ aip-stack/providers/aws/admin/admin.tf
@@ -114,7 +114,7 @@ module "s3bucket" {
     region = "${var.region}"
     accountno = "${var.accountno}"
 	profile = "${var.profile}"
-	localpath = "./admin/config"
+	localpath = "./admin/ecs-configs"
 }
 
 module "ecs" {
