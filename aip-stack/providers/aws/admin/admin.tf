c42f0ff0b867309d789756b96d741fd5d18af8cc
Rakesh Raghavan <rakesh.raghavan@abbott.com>
2016-08-25 10:48:43 -0700

    Added bio ecs service

--- aip-stack/providers/aws/admin/admin.tf
+++ aip-stack/providers/aws/admin/admin.tf
@@ -370,3 +370,26 @@ module "crowd_ecs_service" {
 	ecs_service_dns_name    = "${var.adm_ecs_service_crowd_dns_name}"
 
 }
+
+module "bio_ecs_service" {
+	
+	source = "../../../modules/aws/ecs-service"
+
+	ecs_cluser_id        = "${module.ecs.cluster_id}"
+
+	ecs_service_name     = "${var.adm_ecs_service_bio_service_name}"
+	ecs_task_name        = "${var.adm_ecs_service_bio_task_name}"
+	task_definition_file = "${var.adm_ecs_service_bio_task_definition_file}"
+
+	vpc_id         = "${var.adm_vpc_id}"
+	subnet_ids     = "${module.vpc.subnet_ids}"
+
+	ecs_service_elb_listener_instance_port     = "${var.adm_ecs_service_bio_elb_listener_instance_port}"
+	ecs_service_elb_ssl_cert_arn               = "${var.adm_ecs_service_bio_elb_ssl_cert_arn}"
+	ecs_service_elb_health_check_url      = "${var.adm_ecs_service_bio_elb_health_check_url}"
+	ecs-service-instance-security-group-id  = "${module.ecs.cluster_instance_sg_id}"
+
+	ecs_service_dns_zone_id = "${var.adm_apps_ecs_service_zone_id}"
+	ecs_service_dns_name    = "${var.adm_ecs_service_bio_dns_name}"
+
+}
